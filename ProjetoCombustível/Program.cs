13fb35d183b394a0d0f8797b23cdd62dd2bcdd81
Usuario <Usuario@CPD03-M06>
2024-02-27 11:07:12 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoCombustível/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCombustível
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do combustível: ");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a distância percorrida: ");
+            double d = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a distância percorrida por litro: ");
+            double l = double.Parse(Console.ReadLine());
+
+            double resultado = (d / l) * c;
+            
+            Console.WriteLine("O total gasto foi: R$" + resultado);
+
+            Console.ReadKey();
+
+        }
+    }
+}
